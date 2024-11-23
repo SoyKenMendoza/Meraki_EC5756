67295e037f3f1fec2343ad5ffa5f80047327da89
Ken <ken.alemendoza@gmail.com>
2024-11-23 19:54:13 -0400

    Meraki
    Esta primera version del archivo lista todas las organizaciones de Meraki a las que se tiene acceso con el API Key.

---
+++ p2meraki.py
@@ -0,0 +1,21 @@
+import requests
+
+from pprint import pprint
+
+#Global Parameters
+
+url = 'https://api.meraki.com/api/v1/organizations/'
+
+payload = None
+
+API_KEY = '75dd5334bef4d2bc96f26138c163c0a3fa0b5ca6'
+
+headers = {
+
+    "Authorization": "Bearer 75dd5334bef4d2bc96f26138c163c0a3fa0b5ca6",
+    "Accept": 'application/json'
+}
+
+response = requests.request('GET', url, headers=headers, data = payload)
+
+pprint(response.json())
